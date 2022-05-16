25f006715c21e24ebd1788d00ce7b5b98a299b84
Maximilian Zenke <max.zenke@me.com>
2022-05-16 18:31:03 +0200

    Added documentation for borderBehavior class

--- src/borderBehavior.java
+++ src/borderBehavior.java
@@ -1,4 +1,14 @@
+/**
+ * abstract class used to later specify borderbehavior when convolving image
+ */
 public abstract class borderBehavior {
+    /**
+     * method to return pixel value of given image at given coordinate
+     * @param i x coordinate in 2d integer array representation of PGM image
+     * @param j y coordinate in 2d integer array representation of PGM image
+     * @param image 2d integer array representation of PGM image
+     * @return integer value of pixel at given coordinate
+     */
     public abstract int getPixelValue(int i, int j,  int[][]image);
-    //todo
+
 }
