f1150bbb6cc03b016529386596c6c9e9c401523e
Maximilian Zenke <max.zenke@me.com>
2022-05-16 18:39:02 +0200

    Added documentation for clampingborderBehavior class

--- src/ClampingBorderBehavior.java
+++ src/ClampingBorderBehavior.java
@@ -1,12 +1,22 @@
+/**
+ * specification of abstract class borderBehavior realizing clamping as border behavior
+ */
 public class ClampingBorderBehavior extends borderBehavior{
+    /**
+     * specification of getPixelValue method from abstract parent class.
+     * Clamping = return next edge pixel value for coordinates i, j outside of image
+     *
+     * @param i x coordinate in 2d integer array representation of PGM image
+     * @param j y coordinate in 2d integer array representation of PGM image
+     * @param image 2d integer array representation of PGM image
+     * @return int value of next edge pixel for i, j outside of border
+     */
     @Override
     public int getPixelValue(int i, int j, int[][] image) {
-        //return val of adjacent pixel
-
 
         int width_i = image[0].length-1;
         int height_i = image.length-1;
-
+        //TODO less if statements if possible
         if (i < 0) {
             i = 0;
             }
@@ -19,21 +29,16 @@ public class ClampingBorderBehavior extends borderBehavior{
         if (j>width_i){
             j = width_i;
         }
-
         return image[i][j];
     }
 
+    //TODO delete main
     public static void main(String[] args) {
 
         int[][] ara = {{5,10,14,10,2},
                         {7,9,12, 0, 5},
                         {-1,12,2, 0, 4}};
         System.out.println(ara[0][ara[0].length-1]);
-
         System.out.println(new ClampingBorderBehavior().getPixelValue(3, 2, ara));
-
-
-
-
     }
 }
